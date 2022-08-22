6541d5108029b56c2b94e9e5633f9fdc0b66bf41
Santiago <samf0794@gmail.com>
2022-08-22 10:24:09 -0400

    adding basic routing. ref sn

--- api/main.js
+++ api/main.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+const booksRouter = require('./routes/booksRoutes')
 
 const port = process.env.PORT || 3000
+app.use('/api', booksRouter)
 
 app.listen(port, (req, res) => {
     console.log(`Server running on port ${port}`)
