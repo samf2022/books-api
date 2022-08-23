b2fd781820b3c513faf81a31c99a9bf56394d16b
Santiago <samf0794@gmail.com>
2022-08-23 18:48:51 -0400

    Adding mongo database service. ref sn

--- api/main.js
+++ api/main.js
@@ -1,10 +1,19 @@
 const express = require('express')
-const app = express()
 const booksRouter = require('./routes/booksRoutes')
+const mongoose = require('mongoose')
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require('./config/config')
+const app = express()
+
+mongoose.connect(
+    `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`)
+    .then(() => console.log("Succesfully connected to DB"))
+    .catch((e) => console.log(e))
+
 
 const port = process.env.PORT || 3000
 app.use('/api', booksRouter)
 
-app.listen(port, (req, res) => {
+app.listen(3000, () => {
     console.log(`Server running on port ${port}`)
-})
+ })
+
